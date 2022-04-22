0cf6394cc2d48b37882e0dc2d4f8c139978cc8b0
Ruanh <79330021+joseruanh@users.noreply.github.com>
2022-04-22 10:33:01 -0300

    adicionando área de noticia

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../../estilos/home.css'
+import png from '../../images/png.jpg'
 
 export default function Inicio(){
     return(
@@ -8,10 +9,16 @@ export default function Inicio(){
            <div className="main">
                <div className="veja">
                    <h1>Veja as Rodadas</h1>
-                   <Link to='/rodadas'><button className="botaoVER">Rodadas</button></Link>
+                   <Link to='/rodadas'><button className="botaoVER">Ver</button></Link>
                </div>
                <div></div>
            </div>
+           <div className="noticia">
+                    <p className="tituloNoticia">
+                        'Time' ganhador do primeiro split se prepara para o MSI
+                    </p>
+                    <img src={png} id="painIMG" />
+               </div>
         </main>
     );
 };
