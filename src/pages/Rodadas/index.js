30128b2a69f6cecaa2d70dc089e6463c6c5d5ab4
Ruanh <79330021+joseruanh@users.noreply.github.com>
2022-06-12 22:34:31 -0300

    postando resultados

--- src/pages/Rodadas/index.js
+++ src/pages/Rodadas/index.js
@@ -61,33 +61,33 @@ export default function Rodadas(){
                     <p>rodada 2</p>
 
                     <div className="partida">
-                        <div> <img src={fur} className="logo"/> </div>
+                        <div> <img src={fur} className="logo"/> 1 </div>
                         <div> <img src={vs} className="logo" /> </div>
-                        <div> <img src={lib} className="logo" /> </div>
+                        <div> 0 <img src={lib} className="logo" /> </div>
                     </div>
 
                     <div className="partida">
-                        <div> <img src={kbm} className="logo"/> </div>
+                        <div> <img src={kbm} className="logo"/> 1 </div>
                         <div> <img src={vs} className="logo" /> </div>
-                        <div> <img src={fla} className="logo" /> </div>
+                        <div> 0 <img src={fla} className="logo" /> </div>
                     </div>
 
                     <div className="partida">
-                        <div> <img src={rng} className="logo"/> </div>
+                        <div> <img src={rng} className="logo"/> 0 </div>
                         <div> <img src={vs} className="logo" /> </div>
-                        <div> <img src={red} className="logo" /> </div>
+                        <div> 1 <img src={red} className="logo" /> </div>
                     </div>
 
                     <div className="partida">
-                        <div> <img src={net} className="logo"/> </div>
+                        <div> <img src={net} className="logo"/> 1 </div>
                         <div> <img src={vs} className="logo" /> </div>
-                        <div> <img src={itz} className="logo" /> </div>
+                        <div> 0 <img src={itz} className="logo" /> </div>
                     </div>
 
                     <div className="partida">
-                        <div> <img src={lll} className="logo"/> </div>
+                        <div> <img src={lll} className="logo"/> 1 </div>
                         <div> <img src={vs} className="logo" /> </div>
-                        <div> <img src={png} className="logo" /> </div>
+                        <div> 0 <img src={png} className="logo" /> </div>
                     </div>
 
                 </div>
