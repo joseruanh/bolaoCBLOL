a5fc4d3d2c7d5f1464798f3fa76875dc75ec9476
Ruanh <79330021+joseruanh@users.noreply.github.com>
2022-06-19 17:55:34 -0300

    resultado jogo 5

--- src/pages/Planilha/index.js
+++ src/pages/Planilha/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import '../../estilos/planilha.css'
 
 export default function Planilha(){
-    const primeiro = {nome: 'Jean', pontos1: 4, pontos2: 3, pontos3: 3, pontos4: 2}
-    const segundo = {nome: 'Pickles', pontos1: 4, pontos2: 3, pontos3: 4, pontos4: 4}
+    const primeiro = {nome: 'Jean', pontos1: 4, pontos2: 3, pontos3: 3, pontos4: 3}
+    const segundo = {nome: 'Pickles', pontos1: 4, pontos2: 3, pontos3: 4, pontos4: 5}
     const terceiro = {nome: 'Relâmpago', pontos1: 3, pontos2: 3, pontos3: 3, pontos4: 3}
-    const quarto = {nome: 'Ruanh', pontos1: 3, pontos2: 4, pontos3: 3, pontos4: 3}
+    const quarto = {nome: 'Ruanh', pontos1: 3, pontos2: 4, pontos3: 3, pontos4: 4}
     const quinto = {nome: 'Brener', pontos1: 4, pontos2: 1, pontos3: 3, pontos4: 3}
     const sexto = {nome: 'Kennyd', pontos1: 2, pontos2: 4, pontos3: 3, pontos4: 0}
-    const setimo = {nome: 'Ademiro', pontos1: 2, pontos2: 5, pontos3: 0, pontos4: 1}
+    const setimo = {nome: 'Ademiro', pontos1: 2, pontos2: 5, pontos3: 0, pontos4: 2}
     const oitavo = {nome: 'Victor', pontos1: 0, pontos2: 0, pontos3: 0, pontos4: 0}
-    const nono = {nome: 'Tony', pontos1: 2, pontos2: 4, pontos3: 0, pontos4: 2}
+    const nono = {nome: 'Tony', pontos1: 2, pontos2: 4, pontos3: 0, pontos4: 3}
 
     var jean = primeiro.pontos1 + primeiro.pontos2 + primeiro.pontos3 + primeiro.pontos4;
     var pickles = segundo.pontos1 + segundo.pontos2 + segundo.pontos3 + segundo.pontos4;
@@ -141,31 +141,31 @@ export default function Planilha(){
                             <p>Rodada 4</p>
                             <table className="tabelaPLANILHA">
                                 <tr>
-                                    <td className="destaque">{primeiro.nome}</td> <td id="win">LLL</td> <td>KBM</td> <td id="win">FLA</td> <td>RED</td> <td>NET</td> <td className="destaque">Pontos: {primeiro.pontos4}</td>
+                                    <td className="destaque">{primeiro.nome}</td> <td id="win">LLL</td> <td>KBM</td> <td id="win">FLA</td> <td>RED</td> <td id="win">NET</td> <td className="destaque">Pontos: {primeiro.pontos4}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{segundo.nome}</td> <td id="win">LLL</td> <td id="win">PNG</td> <td id="win">FLA</td> <td id="win">FUR</td> <td>NET</td> <td className="destaque">Pontos: {segundo.pontos4}</td>
+                                    <td className="destaque">{segundo.nome}</td> <td id="win">LLL</td> <td id="win">PNG</td> <td id="win">FLA</td> <td id="win">FUR</td> <td id="win">NET</td> <td className="destaque">Pontos: {segundo.pontos4}</td>
                                 </tr>
                                 <tr>
                                     <td className="destaque">{terceiro.nome}</td> <td id="win">LLL</td> <td id="win">PNG</td> <td>ITZ</td> <td id="win">FUR</td> <td>RNG</td> <td className="destaque">Pontos: {terceiro.pontos4}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{quarto.nome}</td> <td id="win">LLL</td> <td id="win">PNG</td> <td id="win">FLA</td> <td>RED</td> <td>NET</td> <td className="destaque">Pontos: {quarto.pontos4}</td>
+                                    <td className="destaque">{quarto.nome}</td> <td id="win">LLL</td> <td id="win">PNG</td> <td id="win">FLA</td> <td>RED</td> <td id="win">NET</td> <td className="destaque">Pontos: {quarto.pontos4}</td>
                                 </tr>
                                 <tr>
                                     <td className="destaque">{quinto.nome}</td> <td id="win">LLL</td> <td>KBM</td> <td id="win">FLA</td> <td id="win">FUR</td> <td>RNG</td> <td className="destaque">Pontos: {quinto.pontos4}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{sexto.nome}</td> <td>-</td> <td>-</td> <td>-</td> <td>-</td> <td>pt5</td> <td className="destaque">Pontos: {sexto.pontos4}</td>
+                                    <td className="destaque">{sexto.nome}</td> <td>-</td> <td>-</td> <td>-</td> <td>-</td> <td>-</td> <td className="destaque">Pontos: {sexto.pontos4}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{setimo.nome}</td> <td>LIB</td> <td>KBM</td> <td>ITZ</td> <td id="win">FUR</td> <td>NET</td> <td className="destaque">Pontos: {setimo.pontos4}</td>
+                                    <td className="destaque">{setimo.nome}</td> <td>LIB</td> <td>KBM</td> <td>ITZ</td> <td id="win">FUR</td> <td id="win">NET</td> <td className="destaque">Pontos: {setimo.pontos4}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{oitavo.nome}</td> <td>-</td> <td>-</td> <td>-</td> <td>-</td> <td>pt5</td> <td className="destaque">Pontos: {oitavo.pontos4}</td>
+                                    <td className="destaque">{oitavo.nome}</td> <td>-</td> <td>-</td> <td>-</td> <td>-</td> <td>-</td> <td className="destaque">Pontos: {oitavo.pontos4}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{nono.nome}</td> <td>-</td> <td id="win">PNG</td> <td id="win">FLA</td> <td>RED</td> <td>NET</td> <td className="destaque">Pontos: {nono.pontos4}</td>
+                                    <td className="destaque">{nono.nome}</td> <td>-</td> <td id="win">PNG</td> <td id="win">FLA</td> <td>RED</td> <td id="win">NET</td> <td className="destaque">Pontos: {nono.pontos4}</td>
                                 </tr>
                             </table>
                         </div>
--- src/pages/Rodadas/index.js
+++ src/pages/Rodadas/index.js
@@ -155,9 +155,9 @@ export default function Rodadas(){
                     </div>
 
                     <div className="partida">
-                        <div> <img src={net} className="logo"/> </div>
+                        <div> <img src={net} className="logo"/>&#10062;</div>
                         <div> <img src={vs} className="logo" /> </div>
-                        <div> <img src={rng} className="logo" /> </div>
+                        <div>&#10060;<img src={rng} className="logo" /> </div>
                     </div>
 
                 </div>
