de12bf4ecd7146beced02690cbe20c07d7b815a4
Ruanh <79330021+joseruanh@users.noreply.github.com>
2022-07-03 13:06:28 -0300

    subindo rodada

--- src/pages/Planilha/index.js
+++ src/pages/Planilha/index.js
@@ -265,25 +265,25 @@ export default function Planilha(){
                                     <td className="destaque">{primeiro.nome}</td> <td>pt1</td> <td>pt2</td> <td>pt3</td> <td>pt4</td> <td>pt5</td> <td className="destaque">Pontos: {primeiro.pontos8}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{segundo.nome}</td> <td>pt1</td> <td>pt2</td> <td>pt3</td> <td>pt4</td> <td>pt5</td> <td className="destaque">Pontos: {segundo.pontos8}</td>
+                                    <td className="destaque">{segundo.nome}</td> <td>KBM</td> <td>RED</td> <td>PNG</td> <td>LLL</td> <td>FUR</td> <td className="destaque">Pontos: {segundo.pontos8}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{terceiro.nome}</td> <td>pt1</td> <td>pt2</td> <td>pt3</td> <td>pt4</td> <td>pt5</td> <td className="destaque">Pontos: {terceiro.pontos8}</td>
+                                    <td className="destaque">{terceiro.nome}</td> <td>KBM</td> <td>RED</td> <td>PNG</td> <td>LLL</td> <td>FUR</td> <td className="destaque">Pontos: {terceiro.pontos8}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{quarto.nome}</td> <td>pt1</td> <td>pt2</td> <td>pt3</td> <td>pt4</td> <td>pt5</td> <td className="destaque">Pontos: {quarto.pontos8}</td>
+                                    <td className="destaque">{quarto.nome}</td> <td>KBM</td> <td>RED</td> <td>PNG</td> <td>LLL</td> <td>FUR</td> <td className="destaque">Pontos: {quarto.pontos8}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{quinto.nome}</td> <td>pt1</td> <td>pt2</td> <td>pt3</td> <td>pt4</td> <td>pt5</td> <td className="destaque">Pontos: {quinto.pontos8}</td>
+                                    <td className="destaque">{quinto.nome}</td> <td>KBM</td> <td>RED</td> <td>ITZ</td> <td>LLL</td> <td>FLA</td> <td className="destaque">Pontos: {quinto.pontos8}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{sexto.nome}</td> <td>pt1</td> <td>pt2</td> <td>pt3</td> <td>pt4</td> <td>pt5</td> <td className="destaque">Pontos: {sexto.pontos8}</td>
+                                    <td className="destaque">{sexto.nome}</td> <td>KBM</td> <td>RED</td> <td>PNG</td> <td>LLL</td> <td>FUR</td> <td className="destaque">Pontos: {sexto.pontos8}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{setimo.nome}</td> <td>pt1</td> <td>pt2</td> <td>pt3</td> <td>pt4</td> <td>pt5</td> <td className="destaque">Pontos: {setimo.pontos8}</td>
+                                    <td className="destaque">{setimo.nome}</td> <td>KBM</td> <td>RED</td> <td>ITZ</td> <td>LLL</td> <td>FLA</td> <td className="destaque">Pontos: {setimo.pontos8}</td>
                                 </tr>
                                 <tr>
-                                    <td className="destaque">{oitavo.nome}</td> <td>pt1</td> <td>pt2</td> <td>pt3</td> <td>pt4</td> <td>pt5</td> <td className="destaque">Pontos: {oitavo.pontos8}</td>
+                                    <td className="destaque">{oitavo.nome}</td> <td>KBM</td> <td>RED</td> <td>PNG</td> <td>NET</td> <td>FUR</td> <td className="destaque">Pontos: {oitavo.pontos8}</td>
                                 </tr>
                             </table>
                         </div>
